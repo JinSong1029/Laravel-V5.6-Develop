5516f1dff45a3c0a2ca14f8d4018bf8e46bcae7a
Arnold Kim <xdev4420@gmail.com>
2019-02-07 15:16:19 +0100

    Create news finished

--- resources/views/news/create.blade.php
+++ resources/views/news/create.blade.php
@@ -2,4 +2,16 @@
 
 @section('content')
     <h1>This is news create page</h1>
+    <form action="{{ route('news.store') }}" method="post">
+      @csrf
+      <div class="form-group">
+        <label for="title">Title:</label>
+        <input type="text" class="form-control" id="title" placeholder="Enter title" name="title">
+      </div>
+      <div class="form-group">
+        <label for="content">Password:</label>
+        <input type="text" class="form-control" id="content" placeholder="Enter content" name="content">
+      </div>
+      <button type="submit" class="btn btn-primary">Submit</button>
+    </form>
 @endsection
