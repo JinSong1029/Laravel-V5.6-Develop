cc5a77fb7a2563089e0f796f9c2820093fe212a0
brucelee12345567 <brucelee12345567@outlook.com>
2019-02-07 08:54:21 -0800

    newtopic.php

--- app/Http/Controllers/NewsController.php
+++ app/Http/Controllers/NewsController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use App\Newtopic;
 
 class NewsController extends Controller
 {
@@ -14,8 +15,8 @@ class NewsController extends Controller
      */
     public function index()
     {
-        //
-        return view('news.index');
+        $news = Newtopic::all();
+        return view('news.index',['news'=>$news]);
     }
 
     /**
